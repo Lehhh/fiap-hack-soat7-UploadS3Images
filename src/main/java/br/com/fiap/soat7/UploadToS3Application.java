e601dd0f9393992aa44b42c88788c52991439796
Leandro Shiniti Tacara <lst@MacBook-Pro-de-Leandro.local>
2025-01-16 10:20:36 +0800

    execute upload video in S3

--- src/main/java/br/com/fiap/soat7/UploadToS3Application.java
+++ src/main/java/br/com/fiap/soat7/UploadToS3Application.java
@@ -31,15 +31,15 @@ public class UploadToS3Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		List<String> queue = redisService.fetchQueueVideoS3();
-		queue.forEach(q -> {
+		List<String> queueImageS3 = redisService.fetchQueueImageS3();
+		queueImageS3.forEach(q -> {
 			String[] values = q.split(":");
 			String userId = values[0];
 			String videoId = values[1];
 			String version = values[3];
 			List<MultipartFile> multipartFiles = diskUtils.listFilesAsMultipartFile(Path.of(String.format(diskFolder, userId, videoId, version)));
-			List<MultipartFile> multipartFilesVideo = multipartFiles.stream().filter(m -> !m.getOriginalFilename().contains("images") && !m.getOriginalFilename().startsWith(".")).toList();
-			fileUploadService.uploadS3Video(userId, videoId, version,multipartFilesVideo.get(0));
+			List<MultipartFile> multipartFilesZip = multipartFiles.stream().filter(m -> m.getOriginalFilename().contains(".zip") && !m.getOriginalFilename().startsWith(".")).toList();
+			fileUploadService.uploadS3Image(userId, videoId, version, multipartFilesZip.get(0));
 		});
 	}
 }
