92c5322024d3291d16d0ac81336e1b807c696941
Leandro Shiniti Tacara <leandro.tacara@hotmail.com>
2025-02-10 22:27:38 -0300

    add workflow

--- src/main/java/br/com/fiap/soat7/application/usecases/UploadCase.java
+++ src/main/java/br/com/fiap/soat7/application/usecases/UploadCase.java
@@ -7,18 +7,19 @@ import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 @Log4j2
-public class UploadCase implements CommandLineRunner {
+public class UploadCase  {
 
 	private final UploadToS3Properties props;
 	private final UploadS3Service uploadS3Service;
 
-	@Override
-	public void run(String... args) throws Exception {
+	@Scheduled(fixedDelay = 3000)
+	public void executeUploadVideoS3() throws Exception {
 		if (props.getStep().equals(Step.VIDEO.name())) {
 			log.info("Iniciando execução da fila de upload videos");
 			uploadS3Service.uploadVideo();
